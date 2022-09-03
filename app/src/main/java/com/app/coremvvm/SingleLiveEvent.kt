9b958f3b7b056cfaeeeedb42f91b7fc466cdcd5b
azamat.nurkhojayev <azamat.nurkhojayev@homecredit.kz>
2022-09-03 21:47:48 +0600

    SingleLiveEvent

---
+++ app/src/main/java/com/app/coremvvm/SingleLiveEvent.kt
@@ -0,0 +1,28 @@
+package com.app.coremvvm
+
+import androidx.annotation.MainThread
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.atomic.AtomicBoolean
+
+class SingleLiveEvent <T> : MutableLiveData<T>() {
+
+    private var mPending = AtomicBoolean(false)
+
+    @MainThread
+    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
+        super.observe(owner){ t ->
+            if (mPending.compareAndSet(true, false)){
+                observer.onChanged(t)
+            }
+        }
+    }
+
+    @MainThread
+    override fun setValue(value: T) {
+        mPending.set(true)
+        super.setValue(value)
+    }
+
+}
