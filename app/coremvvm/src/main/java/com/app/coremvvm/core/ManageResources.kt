e5d376b778d4e3d5b468321ef79bdcb07cb0f0ac
azamat.nurkhojayev <azamat.nurkhojayev@homecredit.kz>
2022-09-06 15:02:46 +0600

    ManageResources and Dispatchers

---
+++ app/coremvvm/src/main/java/com/app/coremvvm/core/ManageResources.kt
@@ -0,0 +1,15 @@
+package com.app.coremvvm.core
+
+import android.content.Context
+import androidx.annotation.StringRes
+
+interface ManageResources {
+
+    fun string(@StringRes id: Int): String
+
+    class Base(private val context: Context): ManageResources {
+
+        override fun string(id: Int): String = context.getString(id)
+
+    }
+}
